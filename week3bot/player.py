cef3a1f57dd2580fb46378c85128a9d9117def10
Clay Davis <clay.davis.ms@gmail.com>
2024-01-26 21:59:36 -0500

    week3
    week3 bot added

---
+++ week3bot/player.py
@@ -0,0 +1,214 @@
+'''
+Simple example pokerbot, written in Python.
+'''
+from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction, BidAction
+from skeleton.states import GameState, TerminalState, RoundState
+from skeleton.states import NUM_ROUNDS, STARTING_STACK, BIG_BLIND, SMALL_BLIND
+from skeleton.bot import Bot
+from skeleton.runner import parse_args, run_bot
+import random
+import eval7
+import math
+from itertools import combinations
+
+#section on building all hands
+def generate_deck():
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    suits = ['h', 'd', 'c', 's']
+    deck = [rank + suit for rank in ranks for suit in suits]
+    return deck
+
+def generate_all_hands(deck, number):
+    combos = list(combinations(deck, number))
+    return ["".join(c) for c in combos]
+#
+
+hand_to_equity = {
+    "High Card": 0.3,
+    "Pair": 0.6,
+    "Two Pair": 0.8,
+    "Trips": 0.95,
+    "Straight": 0.98,
+    "Flush": 1,
+    "Full House": 1,
+    "Quads": 1,
+    "Straight Flush": 1,
+    "Royal Flush": 1
+}
+
+pocket_2_max, pocket_2_min = 17563648, 327680
+
+deck = generate_deck()
+
+all_hands_2_str = generate_all_hands(deck,2)
+all_hands_2 = [[eval7.Card(hand[:2]) , eval7.Card(hand[2:])] for hand in all_hands_2_str]
+all_hands_3_str = generate_all_hands(deck,3)
+all_hands_3 = [[eval7.Card(hand[:2]) , eval7.Card(hand[2:])] for hand in all_hands_3_str]
+
+pre_flop_fold = []
+def fold_pre_flop(hand):
+    if hand[0].suit != hand[1].suit and hand[0].rank != hand[1].rank and (hand[0].rank + hand[1].rank < 10):
+        print("folded pre-flop since the sum was", hand[0].rank + hand[1].rank)
+        return True
+    return False
+#pre_flop_fold = [[eval7.Card(hand[:2]) , eval7.Card(hand[2:])] for hand in pre_flop_fold]
+def great_preflop(h):
+    if ((h[0].rank == h[1].rank and h[0].rank + h[1].rank >= 14) or h[0].rank + h[1].rank >= 22 or h[0].rank >= 11 or h[1].rank > 10):
+        return True
+    return False
+class Player(Bot):
+    '''
+    A pokerbot.
+    '''
+
+    def __init__(self):
+        '''
+        Called when a new game starts. Called exactly once.
+
+        Arguments:
+        Nothing.
+
+        Returns:
+        Nothing.
+        '''
+        pass
+
+    def handle_new_round(self, game_state, round_state, active):
+        '''
+        Called when a new round starts. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
+        '''
+        my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        my_cards = round_state.hands[active]  # your cards
+        big_blind = bool(active)  # True if you are the big blind
+        pass
+
+    def handle_round_over(self, game_state, terminal_state, active):
+        '''
+        Called when a round ends. Called NUM_ROUNDS times.
+
+        Arguments:
+        game_state: the GameState object.
+        terminal_state: the TerminalState object.
+        active: your player's index.
+
+        Returns:
+        Nothing.
+        '''
+        my_delta = terminal_state.deltas[active]  # your bankroll change from this round
+        previous_state = terminal_state.previous_state  # RoundState before payoffs
+        street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
+        my_cards = previous_state.hands[active]  # your cards
+        opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        pass
+
+    def get_action(self, game_state, round_state, active):
+        '''
+        Where the magic happens - your code should implement this function.
+        Called any time the engine needs an action from your bot.
+
+        Arguments:
+        game_state: the GameState object.
+        round_state: the RoundState object.
+        active: your player's index.
+
+        Returns:
+        Your action.
+        '''
+        # May be useful, but you may choose to not use.
+        legal_actions = round_state.legal_actions()  # the actions you are allowed to take
+        street = round_state.street  # 0, 3, 4, or 5 representing pre-flop, flop, turn, or river respectively
+        my_cards = round_state.hands[active]  # your cards
+        board_cards = round_state.deck[:street]  # the board cards
+        my_pip = round_state.pips[active]  # the number of chips you have contributed to the pot this round of betting
+        opp_pip = round_state.pips[1-active]  # the number of chips your opponent has contributed to the pot this round of betting
+        my_stack = round_state.stacks[active]  # the number of chips you have remaining
+        opp_stack = round_state.stacks[1-active]  # the number of chips your opponent has remaining
+        my_bid = round_state.bids[active]  # How much you bid previously (available only after auction)
+        opp_bid = round_state.bids[1-active]  # How much opponent bid previously (available only after auction)
+        continue_cost = opp_pip - my_pip  # the number of chips needed to stay in the pot
+        my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
+        opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
+        pot = my_contribution + opp_contribution
+        big_blind = bool(active)
+        if RaiseAction in legal_actions:
+           min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
+           min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
+           max_cost = max_raise - my_pip  # the cost of a maximum bet/raise
+           #print(min_raise, max_raise, my_stack, opp_stack, my_pip, opp_pip)
+
+        def bail_option(legal_actions):
+            if CheckAction in legal_actions:
+                return CheckAction()
+            return FoldAction()
+    
+        #___myLogic___
+        all_cards = my_cards + board_cards
+        hand = [eval7.Card(s) for s in all_cards]
+        hand_rank = eval7.evaluate(hand)
+        hand_type = eval7.handtype(hand_rank)
+        equity = hand_to_equity[hand_type]
+
+        if BidAction in legal_actions:     #Simply pay the most for the bid due to GT
+            return BidAction(my_stack)
+        
+        #preflop logic
+        if street == 0:
+            if fold_pre_flop(hand):
+                return bail_option(legal_actions)
+            
+            if opp_pip == 2:
+                if fold_pre_flop(hand):
+                    return bail_option(legal_actions)
+                return RaiseAction(6)
+            else:
+                if great_preflop(hand):
+                    if RaiseAction in legal_actions:
+                        return RaiseAction(max_raise)
+                    return CallAction()
+                else:
+                    return bail_option(legal_actions)
+        
+        #postflop logic
+        print(hand_type)
+        if continue_cost > equity*pot:
+            return bail_option(legal_actions)
+        if equity < 0.5:
+            print(hand[0].rank + hand[1].rank)
+            if hand[0].rank + hand[1].rank < 17:
+                if CheckAction in legal_actions:
+                    return CheckAction()
+                return CallAction()
+            return bail_option(legal_actions)
+        else:
+            if equity <= 0.6:
+                if random.random() < 0.2:
+                    if RaiseAction in legal_actions:
+                        return RaiseAction(min(int(0.3 * pot), max_raise))
+                    return CallAction()
+                if random.random() < 0.7:
+                    if CheckAction in legal_actions:
+                        return CheckAction()
+                    return FoldAction()
+                if CheckAction in legal_actions:
+                    return CheckAction()
+                return CallAction()
+            if RaiseAction in legal_actions:
+                return RaiseAction(min(int(0.5 * pot), max_raise))
+            if CallAction in legal_actions:
+                return CallAction()
+            return CheckAction()
+        
+
+
+if __name__ == '__main__':
+    run_bot(Player(), parse_args())
